dec797e9dbd3259d7aa0c70e6ed0e15c2e8d87bf
combiJihoon <combi_jihoon@naver.com>
2021-08-17 22:25:28 +0900

    Make signup api

--- elice_library/views/auth_view.py
+++ elice_library/views/auth_view.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, url_for, flash, session
 from werkzeug.utils import redirect
-from ..forms import RegistrationForm
+from forms import LoginForm, RegistrationForm
 from bcrypt import checkpw, hashpw, gensalt
 from models import *
 
@@ -9,19 +9,21 @@ bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 @bp.route('/login', methods=['GET', 'POST'])
 def login():
-    login_form = RegistrationForm(request.form)
+    login_form = LoginForm(request.form)
+    user_id = login_form.user_id.data
+    user_password = login_form.user_password.data
     if request.method == 'POST' and login_form.validate():
-        user_data = User.query.filter(user_id=login_form.user_id.data).first()
+        user_data = User.query.filter(user_id=user_id).first()
         if not user_data:
-            flash('존재하지 않는 아이디입니다.')
+            flash('아이디를 다시 확인해 주세요.')
             return redirect(url_for('auth.login'))
-        elif not checkpw(login_form.user_password.data.encode('utf-8'), user_data.user_password):
+        elif not checkpw(user_password.encode('utf-8'), user_data.user_password):
             flash('비밀번호가 일치하지 않습니다.')
             return redirect(url_for('auth.login'))
         else:
             session.clear()
-            session['user_id'] = user_data.user_id
-            flash(f'{user_data.user_id}님 안녕하세요!')
+            session['user_id'] = user_id
+            flash(f'{user_data.user_name}님 안녕하세요!')
             return redirect(url_for('main.home'))
     else:
         return render_template('login.html')
@@ -29,4 +31,21 @@ def login():
 
 @bp.route('/signup', methods=['GET', 'POST'])
 def signup():
-    return render_template('signup.html')
+    registration_form = RegistrationForm(request.form)
+    user_id = registration_form.user_id
+    user_password = registration_form.user_password
+    user_name = registration_form.user_name
+    if request.method == 'POST' and registration_form.validate():
+        user_data = User.query.filter_by(user_id=user_id).first()
+        if not user_data:
+            hashed_password = hashpw(user_password.encode('utf-8'), gensalt())
+
+            new_user = User(user_id=user_id,
+                            user_password=hashed_password, user_name=user_name)
+            db.session.add(new_user)
+            db.session.commit()
+        else:
+            flash('이미 존재하는 아이디입니다. 다시 입력해 주세요.')
+            return redirect(url_for('main.signup'))
+    else:
+        return render_template('signup.html')
