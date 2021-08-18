5d79b5327b8cab940afb6407085ee396776c7c2c
combiJihoon <combi_jihoon@naver.com>
2021-08-18 18:00:40 +0900

    Modify the type of user_id with session

--- views/rental_view.py
+++ views/rental_view.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template, url_for, flash, session
+from flask import Blueprint, render_template, url_for, flash, session, g
 from models import Book, Rental
+from app import db
 from werkzeug.utils import redirect
 
 bp = Blueprint('rental', __name__, url_prefix='/rental')
@@ -16,7 +17,7 @@ def record():
 
 @bp.route('/<int:book_id>', methods=['GET', 'POST'])
 def rent(book_id):
-    if session['user_id'] is None:
+    if g.user is None:
         flash('로그인 후 대여할 수 있습니다.')
         return redirect(url_for('main.home'))
     else:
@@ -24,7 +25,7 @@ def rent(book_id):
         user_id = session['user_id']
         if book.stock > 0:
             book.stock -= 1
-            rental = Rental(user_id=user_id, book_id=book_id, returned_at=NULL)
+            rental = Rental(user_id=user_id, book_id=book_id, returned_at=None)
             db.session.add(rental)
             db.session.commit()
             return render_template('rental_record.html')
