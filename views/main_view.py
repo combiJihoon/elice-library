8debacbcd48ab7065ecaf685ee2a82c7a370fe1a
combiJihoon <combi_jihoon@naver.com>
2021-08-18 14:39:14 +0900

    Add book detail api and book_detail.html

--- views/main_view.py
+++ views/main_view.py
@@ -8,3 +8,9 @@ bp = Blueprint('main', __name__, url_prefix='/')
 def home():
     book_list = Book.query.order_by(Book.book_id.asc()).all()
     return render_template('index.html', book_list=book_list)
+
+
+@bp.route('/detail/<int:book_id>', methods=['GET'])
+def book_detail(book_id):
+    book = Book.query.filter_by(book_id=book_id).first()
+    return render_template('index.html', book=book)
