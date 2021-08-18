618eef46fd7fcfe1274b623ad1967180a35142ad
combiJihoon <combi_jihoon@naver.com>
2021-08-18 13:58:54 +0900

    Make main api and index.html

--- views/main_view.py
+++ views/main_view.py
@@ -1,8 +1,10 @@
 from flask import Blueprint, render_template
+from models import Book
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
 
 @bp.route('/', methods=['GET'])
 def home():
-    return render_template('index.html')
+    book_list = Book.query.order_by(Book.book_id.asc()).all()
+    return render_template('index.html', book_list=book_list)
