70e0477990b3b84318b5c061da3eae5841cda822
combiJihoon <combi_jihoon@naver.com>
2021-08-18 16:39:54 +0900

    Edit rent api

--- views/rental_view.py
+++ views/rental_view.py
@@ -5,13 +5,21 @@ from werkzeug.utils import redirect
 bp = Blueprint('rental', __name__, url_prefix='/rental')
 
 
+@bp.route('/record', methods=['GET'])
+def record():
+
+
 @bp.route('/<int:book_id>', methods=['GET', 'POST'])
 def rent(book_id):
-    book = Book.query.filter_by(book_id=book_id).first()
-    if book.stock > 0:
-        book.stock -= 1
-        db.session.commit()
-        return render_template('rental_record.html')
-    else:
-        flash('현재 대여 가능한 책이 없습니다.')
+    if session['user_id'] is None:
+        flash('로그인 후 대여할 수 있습니다.')
         return redirect(url_for('main.home'))
+    else:
+        book = Book.query.filter_by(book_id=book_id).first()
+        if book.stock > 0:
+            book.stock -= 1
+            db.session.commit()
+            return render_template('rental_record.html')
+        else:
+            flash('현재 대여 가능한 책이 없습니다.')
+            return redirect(url_for('main.home'))
