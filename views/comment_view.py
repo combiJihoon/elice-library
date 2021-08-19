5806413251249affacaebf85f16e98c54939687d
combiJihoon <combi_jihoon@naver.com>
2021-08-19 10:53:07 +0900

    Delete create_comment api in comment_view and add it to the book_detail api

--- views/comment_view.py
+++ views/comment_view.py
@@ -5,21 +5,21 @@ from models import Book, Comment, User
 bp = Blueprint('comment', __name__, url_prefix='/comment')
 
 
-@bp.route('/<int:book_id>', methods=['GET', 'POST'])
-def create_comment(book_id):
-    if request.method == 'POST':
-        if g.user is None:
-            flash('로그인 후 사용할 수 있습니다.')
-        else:
-            user_id = session['user_id']
-            rating = request.form['rating']
-            content = request.form['content']
-            comment = Comment(user_id=user_id, book_id=book_id,
-                              rating=rating, content=content)
-            db.session.add(comment)
-            db.session.commit()
-        return redirect(url_for('main.book_detail', book_id=book_id))
+# @bp.route('/<int:book_id>', methods=['GET', 'POST'])
+# def create(book_id):
+#     if request.method == 'POST':
+#         if g.user is None:
+#             flash('로그인 후 사용할 수 있습니다.')
+#         else:
+#             user_id = session['user_id']
+#             rating = request.form['rating']
+#             content = request.form['content']
+#             comment = Comment(user_id=user_id, book_id=book_id,
+#                               rating=rating, content=content)
+#             db.session.add(comment)
+#             db.session.commit()
+#         return redirect(url_for('main.book_detail', book_id=book_id))
 
-    else:
-        comment_list = Comment.query.order_by(Comment.created_at.desc()).all()
-        return render_template('book_detail.html', comment_list=comment_list)
+#     else:
+#         comment_list = Comment.query.order_by(Comment.created_at.desc()).all()
+#         return render_template('book_detail.html', comment_list=comment_list)
--- views/main_view.py
+++ views/main_view.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
-from models import Book
+from flask import Blueprint, request, render_template, url_for, flash, session, g
+from werkzeug.utils import redirect
+from models import Book, Comment
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
@@ -10,7 +11,22 @@ def home():
     return render_template('index.html', book_list=book_list)
 
 
-@bp.route('/detail/<int:book_id>', methods=['GET'])
+@bp.route('/detail/<int:book_id>', methods=['GET', 'POST'])
 def book_detail(book_id):
-    book = Book.query.filter_by(book_id=book_id).first()
-    return render_template('book_detail.html', book=book)
+    if request.method == 'POST':
+        if g.user is None:
+            flash('로그인 후 사용할 수 있습니다.')
+        else:
+            user_id = session['user_id']
+            rating = request.form['rating']
+            content = request.form['content']
+            comment = Comment(user_id=user_id, book_id=book_id,
+                              rating=rating, content=content)
+            db.session.add(comment)
+            db.session.commit()
+        return redirect(url_for('main.book_detail', book_id=book_id))
+
+    else:
+        comment_list = Comment.query.order_by(Comment.created_at.desc()).all()
+        book = Book.query.filter_by(book_id=book_id).first()
+        return render_template('book_detail.html', book=book, comment_list=comment_list)
