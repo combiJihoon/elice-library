3fe014e8c122a674b7a178a38658683b935a0873
combiJihoon <combi_jihoon@naver.com>
2021-08-19 10:31:12 +0900

    Create create_comment api

--- views/comment_view.py
+++ views/comment_view.py
@@ -1,8 +1,25 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, request, render_template, url_for, flash, session, g
+from werkzeug.utils import redirect
+from models import Book, Comment, User
 
 bp = Blueprint('comment', __name__, url_prefix='/comment')
 
 
-@bp.route('/', methods=['GET'])
-def home():
-    return render_template('index.html')
+@bp.route('/<int:book_id>', methods=['GET', 'POST'])
+def create_comment(book_id):
+    if request.method == 'POST':
+        if g.user is None:
+            flash('로그인 후 사용할 수 있습니다.')
+        else:
+            user_id = session['user_id']
+            rating = request.form['rating']
+            content = request.form['content']
+            comment = Comment(user_id=user_id, book_id=book_id,
+                              rating=rating, content=content)
+            db.session.add(comment)
+            db.session.commit()
+        return redirect(url_for('main.book_detail', book_id=book_id))
+
+    else:
+        comment_list = Comment.query.order_by(Comment.created_at.desc()).all()
+        return render_template('book_detail.html', comment_list=comment_list)
