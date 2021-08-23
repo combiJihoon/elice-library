207ec8f0f19811281755b866c943081a8c408cd1
combiJihoon <combi_jihoon@naver.com>
2021-08-23 22:59:09 +0900

    Backup before merge

--- test.py
+++ test.py
@@ -1,23 +1,5 @@
-# book_name, publisher, author, publication_date, pages, isbn, description, link(img_url)
-import csv
-import datetime
+def a(b):
+    return len(b) > 2
 
 
-def push_data():
-    with open('./library.csv', 'r', encoding='UTF-8') as data:
-        reader = csv.DictReader(data)
-        for lines in reader:
-            id = lines['id']
-            img_url = f"./static/images/{id}"
-            try:
-                # png로 열리면 img_url 은 ".png"
-                open(f"./static/images/{id}.png")
-                img_url += ".png"
-            except:
-                img_url += ".jpg"
-
-            print(img_url)
-            # print(lines['book_name'])
-
-
-push_data()
+print(type(a("sdf")))
--- views/comment_view.py
+++ views/comment_view.py
@@ -16,3 +16,24 @@ def delete(comment_id):
 
     flash('정상적으로 삭제 되었습니다.')
     return redirect(url_for('main.detail', book_id=book_id))
+
+
+# @bp.route('/update/<int:comment_id>', methods=['GET'])
+# def update_try(comment_id):
+
+#     return render_template('book_detail.html')
+
+
+# @bp.route('/update/<int:comment_id>', methods=['POST'])
+# def update(comment_id):
+#     content = request.form['content']
+#     if not content:
+#         flash('내용을 작성해 주세요.')
+#         return redirect('comment.update_try', comment_id=comment_id)
+#     comment = Comment.query.filter_by(comment_id=comment_id).first()
+#     book_id = comment.book_id
+
+#     comment.content = content
+
+#     flash('정상적으로 삭제 되었습니다.')
+#     return redirect(url_for('main.detail', book_id=book_id))
