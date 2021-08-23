ad4157e8925142685ade23c189efda9b1a2a2d88
combiJihoon <combi_jihoon@naver.com>
2021-08-23 22:57:58 +0900

    Add 404 error page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 import config
@@ -8,10 +8,8 @@ migrate = Migrate()
 
 
 # 오류 페이지 정의
-'''
 def page_not_found(e):
     return render_template('404.html'), 404
-'''
 
 
 def create_app():
@@ -32,10 +30,8 @@ def create_app():
     app.register_blueprint(comment_view.bp)
     app.register_blueprint(rental_view.bp)
 
-    '''
     # 오류 페이지 등록
     app.register_error_handler(404, page_not_found)
-    '''
 
     return app
 
