ea8ea81cd40c76afbeff338c4cb2bc3d87213ba3
combiJihoon <combi_jihoon@naver.com>
2021-09-04 16:20:40 +0900

    Edit img_url path and figure out the way to upload files

---
+++ elice_library/data.py
@@ -0,0 +1,39 @@
+# book_name, publisher, author, publication_date, pages, isbn, description, link(img_url)
+import csv
+import datetime
+from app import create_app
+from app import db
+from models import Book
+
+app = create_app()
+app.app_context().push()
+
+
+def push_data():
+    if Book.query.first() is None:
+        with app.app_context():
+            with open('library.csv', 'r', encoding='UTF-8') as data:
+                reader = csv.DictReader(data)
+                for lines in reader:
+                    id = lines['id']
+                    img_url = f"static/images/{id}"
+                    try:
+                        # png로 열리면 img_url 은 ".png"
+                        open(f"images/{id}.png")
+                        img_url += ".png"
+                    except:
+                        img_url += ".jpg"
+
+                    # print(img_url)
+                    # print(lines['book_name'])
+
+                    publication_date = datetime.datetime.strptime(
+                        lines['publication_date'], '%Y-%m-%d')
+                    book = Book(book_name=lines['book_name'], publisher=lines['publisher'], author=lines['author'], publicated_at=publication_date, pages=int(
+                        lines['pages']), img_url=img_url, isbn=int(lines['isbn']), description=lines['description'], link=lines['link'], stock=10, rating=0)
+                    db.session.add(book)
+                    db.session.commit()
+                db.create_all(app=app)
+
+
+push_data()
